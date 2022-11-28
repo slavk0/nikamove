ad3cc8faa507cd97357434e4b735dbe32ea4058f
Slavko <slavko.ua@gmail.com>
2022-11-27 21:51:31 -0500

    Updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.onload = moveOut();
 
 function moveOut() {
-    move("#text").set("margin-left", 200).delay("1s").end();
+    move(".left").set("margin-left", 150).delay("1s").end();
+    move(".right").set("margin-right", 150).delay("1s").end();
 }
